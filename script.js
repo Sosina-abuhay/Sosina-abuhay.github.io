8626b1937d78ab43e9a0678720ed8a23c05db2ed
Sosina-abuhay <sabuhay@hamilton.edu>
2022-11-28 20:56:34 -0500

    jquery

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 // when the dog button is clicked, call the ShowDog function
-document.getElementById("dogButton").onclick = ShowDog;
+$("#dogButton").click(function () {
+    console.log("dog button clicked");
+});
 
-// A function that changes the visibility of the dog based on the current state
-function ShowDog(){
-    if (document.getElementById("dog").style.display == "none"){
-        document.getElementById("dog").style.display = "block";
-    }
-    else{
-        document.getElementById("dog").style.display = "none";
-    }
-}
